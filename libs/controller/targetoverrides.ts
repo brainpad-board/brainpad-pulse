ee61359798c8b29df6db728060b6271f7641f9f3
Dat Tran <dat.tran@ghielectronics.com>
2021-10-06 13:51:41 -0400

    update button for controller

--- libs/controller/targetoverrides.ts
+++ libs/controller/targetoverrides.ts
@@ -1,12 +1,12 @@
 namespace controller {
     //% fixedInstance block="left"
-    export const left = new Button(1);
+    export const left = new Button(1, input.buttonLeft.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
     //% fixedInstance block="up"
-    export const up = new Button(2);
+    export const up = new Button(2, input.buttonUp.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
     //% fixedInstance block="right"
-    export const right = new Button(3);
+    export const right = new Button(3, input.buttonRight.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
     //% fixedInstance block="down"
-    export const down = new Button(4);
+    export const down = new Button(4, input.buttonDown.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
 	//% fixedInstance block="A"
     export const A = new Button(5, input.buttonA.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
     //% fixedInstance block="B"

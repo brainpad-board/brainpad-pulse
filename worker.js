605de98fcf504b88fd46411db28ca313339f7911
Dat Tran <dat.tran@ghielectronics.com>
2021-10-20 09:18:38 -0400

    change path

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 /// <reference path="../../built/pxtlib.d.ts"/>
 /// <reference path="../../built/pxtcompiler.d.ts"/>
-importScripts("/brainpad-pulse/pxtworker.js");
+importScripts("/pxtworker.js");
 var pm = postMessage;
 // work around safari not providing bta
 if (typeof btoa === "undefined") {

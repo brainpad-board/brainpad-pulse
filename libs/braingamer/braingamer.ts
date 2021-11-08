011f7cf543c63da22198f1add253b380e2d58360
Dat Tran <dat.tran@ghielectronics.com>
2021-11-08 10:59:33 -0500

    arrange items

--- libs/braingamer/braingamer.ts
+++ libs/braingamer/braingamer.ts
@@ -16,11 +16,72 @@ enum GamerButton {
 //% block="BrainGamer"
 //% weight=70 color="#e15f41" icon="\uf11b"
 namespace braingamer {	
+	
+	/**
+	 * Set sound On, Off
+	 */
+	//% blockId=braingamer_sound block="set sound %on=toggleOnOff"
+	//% weight=99
+    export function Sound(on: boolean): void {
+        if (on) {
+			pins.P0.analogWrite(512)
+			pins.P0.analogSetPeriod(1000)
+		}
+			
+		else
+			pins.P0.analogWrite(0)
+    }	
+	
+	/**
+	 * Play short sound
+	 */
+	//% blockId=braingamer_beep block="Beep"
+	//% weight=98
+    export function Beep(): void {
+		pins.P0.analogWrite(512)
+		pins.P0.analogSetPeriod(1000)
+		pause(100);
+		pins.P0.analogWrite(0)
+    }
+	
+	/**
+	 * Set vibrate
+	 */
+	//% blockId=braingamer_vibrate block="set vibrate %on=toggleOnOff"
+	//% weight=97
+    export function Vibrate(on: boolean): void {
+        if (on)
+			pins.P8.digitalWrite(false)
+		else 
+			pins.P8.digitalWrite(true)
+				
+    } 
+	
+	/**
+	 * Read rocket value
+	 */
+	//% blockId=braingamer_rocket block="rocket %gamerrocket"
+	//% weight=96
+    export function Rocket(gamerrocket: GamerRocket): number {
+        let value = 0;
+		if (gamerrocket == GamerRocket.X) {
+			value = pins.P4.analogRead();
+		}
+		else 
+			value = pins.P3.analogRead();
 		
+		value = Math.map(value, 0, 1024, 0, 100);
+		
+		return value | 0;
+    } 
+
+	
+	
+	
 	/**
 	 * Run some code when a button is pressed or released
 	 */
-	//% weight=99 blockGap=8 help=controller/button/on-event
+	//% weight=95 blockGap=8 help=controller/button/on-event
 	//% blockId=braingamer_keyonevent block="on button %button %event"
 	export function onEvent(button: GamerButton, event: ControllerButtonEvent, handler: () => void) {
 		let id = 0;
@@ -40,7 +101,7 @@ namespace braingamer {
 	/**
 	 * Indicates if the button is currently pressed
 	 */
-	//% weight=96 blockGap=8 help=controller/button/is-pressed
+	//% weight=94 blockGap=8 help=controller/button/is-pressed
 	//% blockId=braingamer_keyispressed block="is %button pressed"	
 	export function isPressed(button: GamerButton) : boolean {
 		let pressed = controller.up.isPressed();
@@ -56,51 +117,5 @@ namespace braingamer {
 		
 		return pressed;
 	}
-			
-	//% blockId=braingamer_rocket block="rocket %gamerrocket"
-    export function Rocket(gamerrocket: GamerRocket): number {
-        let value = 0;
-		if (gamerrocket == GamerRocket.X) {
-			value = pins.P4.analogRead();
-		}
-		else 
-			value = pins.P3.analogRead();
-		
-		value = Math.map(value, 0, 1024, 0, 100);
-		
-		return value | 0;
-    } 
 
-	
-	//% blockId=braingamer_beep block="Beep"
-    export function Beep(): void {
-		pins.P0.analogWrite(512)
-		pins.P0.analogSetPeriod(1000)
-		pause(100);
-		pins.P0.analogWrite(0)
-    }
-
-	//% blockId=braingamer_sound block="set sound %on=toggleOnOff"
-    export function Sound(on: boolean): void {
-        if (on) {
-			pins.P0.analogWrite(512)
-			pins.P0.analogSetPeriod(1000)
-		}
-			
-		else
-			pins.P0.analogWrite(0)
-    }	
-	
-	//% blockId=braingamer_vibrate block="set vibrate %on=toggleOnOff"
-    export function Vibrate(on: boolean): void {
-        if (on)
-			pins.P8.digitalWrite(false)
-		else 
-			pins.P8.digitalWrite(true)
-				
-    } 
-	
-	
-
-	
 }

cfc9a75a32799b0e3ee407628c7cef30bdb4f1a1
Dat Tran <dat.tran@ghielectronics.com>
2021-11-12 15:42:21 -0500

    add icon resource

---
+++ libs/brainpad/players.ts
@@ -0,0 +1,23 @@
+namespace sprites.player {
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="food"
+    export const playersSmartDuck1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="food"
+    export const playersSmartDuck2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="food"
+    export const playersSmartDuck3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="food"
+    export const playersJumper1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="food"
+    export const playersJumper2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="food"
+    export const playersJumper3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="food"
+    export const playersJumper1 = image.ofBuffer(hex``);
+}

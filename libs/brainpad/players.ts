15b72297691fd04d60fdf10bd5026d893b8f5873
Dat Tran <dat.tran@ghielectronics.com>
2021-11-12 15:47:53 -0500

    fixed build error

--- libs/brainpad/players.ts
+++ libs/brainpad/players.ts
@@ -1,23 +1,23 @@
 namespace sprites.player {
     //% fixedInstance jres blockIdentity=images._image
-    //% tags="food"
+    //% tags="players"
     export const playersSmartDuck1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
-    //% tags="food"
+    //% tags="players"
     export const playersSmartDuck2 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
-    //% tags="food"
+    //% tags="players"
     export const playersSmartDuck3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
-    //% tags="food"
+    //% tags="players"
     export const playersJumper1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
-    //% tags="food"
+    //% tags="players"
     export const playersJumper2 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
-    //% tags="food"
+    //% tags="players"
     export const playersJumper3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._image
-    //% tags="food"
-    export const playersJumper1 = image.ofBuffer(hex``);
+    //% tags="players"
+    export const playersJumper4 = image.ofBuffer(hex``);
 }

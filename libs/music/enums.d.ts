77ebd63f5d0c61983d58b23c174bccd11689efb3
Dat Tran <dat.tran@ghielectronics.com>
2021-12-09 09:38:10 -0500

    support more sound pin instead of P0 and builtin

--- libs/music/enums.d.ts
+++ libs/music/enums.d.ts
@@ -2,10 +2,22 @@
 
 
     declare const enum SoundOutputDestination {
-    //% block="pin PWM"
-    PinPWM = 1,
-    //% block="speaker"
+    //% block="Buzzer"
     Speaker = 0,
+    //% block="P0"
+    PinP0 = 1,
+    //% block="P1"
+    PinP1 = 2,
+    //% block="P2"
+    PinP2 = 3,
+    //% block="P3"
+    PinP3 = 4,
+    //% block="P8"
+    PinP8 = 5,
+    //% block="P12"
+    PinP12 = 6,
+    //% block="P16"
+    PinP16 = 7,
     }
 
 // Auto-generated. Do not edit. Really.

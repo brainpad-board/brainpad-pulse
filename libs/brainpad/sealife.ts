5556b7173c8031068dee9add810b37970330b0d8
Greg Norris <greg.norris@ghielectronics.com>
2022-04-15 12:07:17 -0400

    Added addional gallery sprites

---
+++ libs/brainpad/sealife.ts
@@ -0,0 +1,23 @@
+namespace sprites.sealife {
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="sealife"
+    export const sealifeFish1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="sealife"
+    export const sealifeFish2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="sealife"
+    export const sealifeClam = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="sealife"
+    export const sealifeSeaweed1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="sealife"
+    export const sealifeSeaweed2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="sealife"
+    export const sealifeCrab = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="sealife"
+    export const sealifeShrimp = image.ofBuffer(hex``);
+}
